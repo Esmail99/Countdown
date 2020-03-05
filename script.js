9c5a3a1220d08ab398854b9efc80ea8aa045773b
Esmail <elmoussel12@gmail.com>
2020-03-05 18:41:06 +0200

    Adding arrow functions and modifying the code

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// getting user inputs..
+// getting user inputs and HTML elements that we need..
 var button = document.getElementsByTagName('button')[0];
 var eventDate = document.getElementById('eventDate');
 var eventTime = document.getElementById('eventTime');
@@ -9,14 +9,14 @@ var userInputs = document.getElementById('userInputs');
 var eventStarted = document.getElementById('eventStarted');
 
 // get current time since 1970 ..
-// Math.floor() to remove the decimal parts ..
-function currentTimeBySecondsFunction(){
+var currentTimeBySecondsFunction = () => {
     let currentTimeBySeconds = new Date().getTime()/1000;
     return currentTimeBySeconds;
 }
 
 // When clicking on the button set the new date and make the calculations
-button.onclick = (function(){
+var clicking = () =>{
+    // check user inputs..
     if(!eventTitle.value || !eventDate.value || !eventTime.value){
         alert('Please fill all inputs');
     }
@@ -25,30 +25,33 @@ button.onclick = (function(){
         countdown.style.visibility = 'visible';
         // hide the userInputs ..
         userInputs.style.display = 'none'
-        
         // writing event name..
         h1.innerHTML = eventTitle.value;
         //set event Date..
-        var event = new Date(eventDate.value);
+        eventDate = new Date(eventDate.value);
         //set event Time..
         eventTime = eventTime.value;
-        var hours = eventTime.split(':')[0];
-        var minutes = eventTime.split(':')[1];
-        event.setHours(hours,minutes,0,0);
+        const hours = eventTime.split(':')[0];
+        const minutes = eventTime.split(':')[1];
+        eventDate.setHours(hours,minutes,0,0);
         // get event time since 1970 ..
-        function eventTimeBySecondsFunction(){
-            let eventTimeBySeconds = event.getTime()/1000;
+        var eventTimeBySecondsFunction = () =>{
+            const eventTimeBySeconds = eventDate.getTime()/1000;
             return eventTimeBySeconds;
         }
         // get the time left for event
-        function reloadTimer(){
-            let secondsLeft = Math.floor((eventTimeBySecondsFunction()-currentTimeBySecondsFunction())%60);
+        var timeLeftBySeconds = () => {
+            return eventTimeBySecondsFunction()-currentTimeBySecondsFunction();
+        }
+        // Math.floor() to remove the decimal parts ..
+        var reloadTimer = () => {
+            let secondsLeft = Math.floor(timeLeftBySeconds()%60);
             secondsLeft = secondsLeft.toString();
-            let minutesLeft = Math.floor(((eventTimeBySecondsFunction()-currentTimeBySecondsFunction())/60)%60);
+            let minutesLeft = Math.floor((timeLeftBySeconds()/60)%60);
             minutesLeft = minutesLeft.toString();
-            let hoursLeft = Math.floor(((eventTimeBySecondsFunction()-currentTimeBySecondsFunction())/60/60)%24);
+            let hoursLeft = Math.floor((timeLeftBySeconds()/60/60)%24);
             hoursLeft = hoursLeft.toString();
-            let daysLeft = Math.floor((eventTimeBySecondsFunction()-currentTimeBySecondsFunction())/60/60/24);
+            let daysLeft = Math.floor(timeLeftBySeconds()/60/60/24);
             daysLeft = daysLeft.toString();
             if(daysLeft<0){daysLeft='0'}
             if(hoursLeft<0){hoursLeft='0'}
@@ -65,8 +68,6 @@ button.onclick = (function(){
         }
         setInterval(reloadTimer,1000);
     }
-})
+}
 
-// making big changes now !
-// hehe
-// hellos
+button.onclick = (clicking);
